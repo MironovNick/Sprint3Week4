192c59418adfe448c689bb9712e703226a91553d
MironovNick <mironov.nik88@gmail.com>
2024-07-11 12:45:19 +0300

    add new const newState

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -5,15 +5,18 @@ type ActionType =
     | { type: 'check'; payload: number }
 
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+
+    const newState = [...state];
+
     switch (action.type) {
         case 'sort': { // by name
 
-            return state.sort((a: UserType, b: UserType) => action.payload == 'up' ? a.name > b.name ? 1 : -1 :
+            return newState.sort((a: UserType, b: UserType) => action.payload == 'up' ? a.name > b.name ? 1 : -1 :
                 a.name < b.name ? 1 : -1) // need to fix
         }
         case 'check': {
 
-            return state.filter((item: UserType) => item.age >= action.payload) // need to fix
+            return newState.filter((item: UserType) => item.age >= action.payload) // need to fix
         }
         default:
             return state
